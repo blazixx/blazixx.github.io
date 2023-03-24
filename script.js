b93358a119c8d97f2c5e8693615027b143874b22
blazixx <86879015+blazixx@users.noreply.github.com>
2023-03-24 21:28:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,24 +8,79 @@ function toggleMenu(menuId) {
   }
 }
 
-// функция, которая закрывает все меню
-function closeAllMenus() {
-  var menus = document.getElementsByClassName("menu");
-  for (var i = 0; i < menus.length; i++) {
-    menus[i].style.display = "none";
+// функция, которая закрывает меню
+function closeMenu() {
+  var menu = document.getElementsByClassName("menu");
+  for (var i = 0; i < menu.length; i++) {
+    menu[i].style.display = "none";
   }
 }
 
+// функция, которая открывает окно
+function openWindow(windowId) {
+  var window = document.getElementById(windowId);
+  window.style.display = "block";
+}
+
+// функция, которая закрывает окно
+function closeWindow(windowId) {
+  var window = document.getElementById(windowId);
+  window.style.display = "none";
+}
+
 // добавляем обработчик события на кнопки
-var buttons = document.getElementsByTagName("button");
+var buttons = document.querySelectorAll("nav ul li button");
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
     var menuId = this.getAttribute("data-menu-id");
-    toggleMenu(menuId);
+    var windowId = this.getAttribute("data-window-id");
+    if (menuId) {
+      toggleMenu(menuId);
+    }
+    if (windowId) {
+      openWindow(windowId);
+    }
   });
 }
 
-// добавляем обработчик события на крестики
+// добавляем обработчик события на крестик
 var closeIcons = document.getElementsByClassName("close-icon");
 for (var i = 0; i < closeIcons.length; i++) {
-  closeIcons[i
+  closeIcons[i].addEventListener("click", function () {
+    var windowId = this.getAttribute("data-window-id");
+    closeWindow(windowId);
+  });
+}
+
+// добавляем обработчик события на клик вне меню и окон
+window.addEventListener("click", function (event) {
+  var menu = document.getElementsByClassName("menu");
+  var window = document.getElementsByClassName("window");
+  var target = event.target;
+  var clickedInsideMenu = false;
+  var clickedInsideWindow = false;
+
+  for (var i = 0; i < menu.length; i++) {
+    if (target == menu[i] || menu[i].contains(target)) {
+      clickedInsideMenu = true;
+      break;
+    }
+  }
+
+  for (var i = 0; i < window.length; i++) {
+    if (target == window[i] || window[i].contains(target)) {
+      clickedInsideWindow = true;
+      break;
+    }
+  }
+
+  if (!clickedInsideMenu) {
+    closeMenu();
+  }
+
+  if (!clickedInsideWindow) {
+    for (var i = 0; i < window.length; i++) {
+      closeWindow(window[i].getAttribute("id"));
+    }
+  }
+});
